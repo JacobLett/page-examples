9db4ee208f96136d25ac2bbf4a2acef3051e6843
Jacob Lett <jacoblett@bootstrapcreative.com>
2018-01-10 15:34:40 -0500

    Updated pros and cons

--- fixed/js/custom.js
+++ fixed/js/custom.js
@@ -1,1 +1,27 @@
 console.log("custom js loaded");
+
+
+$(document).ready(function() {
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      autoplay:true,
+      nav:true,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+              nav:true
+          },
+          600:{
+              items:1,
+              nav:true
+          },
+          1000:{
+              items:1,
+              nav:true,
+              loop:false
+          }
+      }
+  })
+});
